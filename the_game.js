3023ffb2a856f4229cab650a8c4d1cf15a9267ee
mihkoo <mikulenaitevaiva@gmail.com>
2024-01-13 23:25:23 +0000

    add playRound function

--- the_game.js
+++ the_game.js
@@ -14,5 +14,34 @@ const options = ['rock', 'paper', 'scissors'];
 const computerSelection = getComputerChoice(options);
 console.log(computerSelection);
 
-let playerSelection = prompt("Rock, paper or scissors?");
+let playerChoice = prompt("Rock, paper or scissors?");
+let playerSelection = playerChoice.toLowerCase();
 console.log(playerSelection);
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        return "It's a draw! Play again";
+    }
+    else if ((playerSelection == "rock") && (computerSelection == "paper")) {
+        return "You lose! Paper beats rock";
+    }
+    else if ((playerSelection == "paper") && (computerSelection == "scissors")) {
+        return "You lose! Scissors beat Paper";
+    }
+    else if ((playerSelection == "scissors") && (computerSelection == "rock")) {
+        return"You lose! Rock beats Scissors";
+    }
+
+    else return "You win!";
+}
+
+// console.log(playRound(playerSelection, computerSelection));
+
+// function game() {
+//     for (let i = 0; i < 5; i++) {
+//         playRound;
+//         console.log(playRound);
+//     } 
+// }
+
+// console.log(game);
