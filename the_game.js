f9975ed4e221a335eee7acf2dc745c57e186e04d
mihkoo <mikulenaitevaiva@gmail.com>
2024-10-11 12:51:38 +0100

    add multiple round functionality

--- the_game.js
+++ the_game.js
@@ -9,39 +9,84 @@ function getComputerChoice(arr) {
     return item;
 }
 
-const options = ['rock', 'paper', 'scissors'];
-
-const computerSelection = getComputerChoice(options);
-console.log(computerSelection);
-
-let playerChoice = prompt("Rock, paper or scissors?");
-let playerSelection = playerChoice.toLowerCase();
-console.log(playerSelection);
-
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection == computerSelection) {
-        return "It's a draw! Play again";
+function playRound(humanSelection, computerSelection) {
+    if (humanSelection === computerSelection) {
+        //return "It's a draw! Play again";
     }
-    else if ((playerSelection == "rock") && (computerSelection == "paper")) {
-        return "You lose! Paper beats rock";
+    else if ((humanSelection === "rock") && (computerSelection === "paper")) {
+        //console.log("You lose! Paper beats rock");
+        return false;
     }
-    else if ((playerSelection == "paper") && (computerSelection == "scissors")) {
-        return "You lose! Scissors beat Paper";
+    else if ((humanSelection === "paper") && (computerSelection === "scissors")) {
+        //console.log("You lose! Scissors beat Paper");
+        return false;
     }
-    else if ((playerSelection == "scissors") && (computerSelection == "rock")) {
-        return"You lose! Rock beats Scissors";
+    else if ((humanSelection === "scissors") && (computerSelection === "rock")) {
+        //console.log("You lose! Rock beats Scissors");
+        return false;
     }
 
-    else return "You win!";
+    else {
+        //console.log("You win!");
+        return true;
+    }
 }
 
-// console.log(playRound(playerSelection, computerSelection));
+//console.log(playRound(humanSelection, computerSelection));
 
-// function game() {
-//     for (let i = 0; i < 5; i++) {
-//         playRound;
-//         console.log(playRound);
-//     } 
-// }
+function playGame() {
 
-// console.log(game);
+    let i;
+    let humanScore = 0;
+    let computerScore = 0;
+    const options = ['rock', 'paper', 'scissors'];
+
+
+
+    for (i = 0; i < 5; i++) {
+
+        const computerSelection = getComputerChoice(options);
+        console.log(computerSelection);
+    
+        let humanChoice = prompt("Rock, paper or scissors?");
+        let humanSelection = humanChoice.toLowerCase();
+        console.log(humanSelection);
+
+        let roundResult = playRound(humanSelection, computerSelection);
+
+        if(roundResult === true) {
+            humanScore+=1;
+            i+1;
+            console.log("You won the round!");
+        }
+        else if (roundResult === false) {
+            computerScore+=1;
+            i+1;
+            console.log("You lost the round!");
+        }
+        else {
+            return "Something went wrong";
+        }
+    }
+
+    if(computerScore>humanScore) {
+        console.log("Computer wins the game!");
+    }
+
+    else if(humanScore>computerScore) {
+        console.log("You won the game!");
+    }
+
+    else {
+        return "whoopsie something messed up (me)";
+    }
+  
+console.log(humanScore, computerScore);
+
+}
+
+playGame();
+
+
+
+
